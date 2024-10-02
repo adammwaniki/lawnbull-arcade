6f7e7568f3b3f4fd0b2df89119008f8c74057b66
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-02 23:25:14 +0300

    Adds animation to landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,14 +1,18 @@
 import { ParticlesBackground } from "./ParticlesBackground"
+import Lottie from "lottie-react";
+import animationData from "../assets/lawnbull-running-animation.json"
+
 
 export default function LandingPage() {
     return (
-      <div className="flex items-center justify-center min-h-screen min-w-full ">
+      <div className="flex flex-col items-center justify-center min-h-screen min-w-full ">
         <ParticlesBackground />
-        <div className="lawnbull-intro z-10">
+        <div className=" lawnbull-intro z-10">
           <h1 className="text-[6rem] lg:text-[10vw] text-white text-center font-wunderbar ">
             Lawnbull Arcade
           </h1>
         </div>
+        <Lottie animationData={animationData} className="h-[40vh]"/>
       </div>
     )
   }
