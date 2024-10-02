cce13212b059d8a950cf1d3a6aa66a2eeaa42edc
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-03 00:22:20 +0300

    Adds navbar to tree

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
 import LandingPage from "./components/LandingPage"
+import About from "./components/AboutPage"
+import MarketingPage from "./components/MarketingPage"
 
 export default function App() {
   return (
-    <div>
-      <LandingPage />
-      
-      
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/marketing" element={<MarketingPage />} />
+        </Routes>
     </div>
+    </Router>
   )
 }
---
+++ client/src/components/MarketingPage.jsx
@@ -0,0 +1,6 @@
+
+export default function MarketingPage() {
+  return (
+    <div>MarketingPage</div>
+  )
+}
