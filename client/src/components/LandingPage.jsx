4ac76849c6d0d380e0c81cc0c8cc950113221a12
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-03 21:54:01 +0300

    Changes bull animation on landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import { ParticlesBackground } from "./ParticlesBackground"
 import Lottie from "lottie-react";
-import animationData from "../assets/lawnbull-running-animation.json"
+import animationData from "../assets/bull-flexing-animation.json"
 import Navbar from "./Navbar"
 
 export default function LandingPage() {
@@ -10,7 +10,7 @@ export default function LandingPage() {
           <ParticlesBackground />
           <div className=" lawnbull-intro z-10">
             <h1 className="text-[6rem] lg:text-[10vw] text-white text-center font-wunderbar ">
-              Lawnbull Arcade
+              Lawnbull Limited
             </h1>
           </div>
           <Lottie animationData={animationData} className="h-[40vh]"/>
