2ef3565804b8fe39043131f8c33472e6827eb26c
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-03 15:57:45 +0300

    Completes responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,39 @@
+import { useState, useEffect } from 'react';
 
 export default function Navbar() {
-  return (
-    <div className="flex w-fit lg:h-fit p-2 bg-gray-200 text-lg lg:text-[3.5vh] lg:leading-tight font-spicy-rice ">
-      <aside>
-        
+  const [isVisible, setIsVisible] = useState(false);
 
-        <div className="flex border-l flex-grow">
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className={`transition-all duration-1000 ease-out transform ${isVisible ? '-translate-x-0' : 'translate-x-full'} flex lg:h-fit pl-2 pt-2 pb-2 text-lg md:text-[3.5vh] lg:leading-tight font-spicy-rice text-white text-center`}>
+      <aside>
+        <div className="flex flex-grow">
           <nav>
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/about">About Us</a>
-              </li>
-              <li>
-                <a href="/marketing">Marketing</a>
-              </li>
-              <li>
-                <a href="/login">Login</a>
-              </li>
+            <ul className="space-y-2">
+              {['Home', 'About', 'Marketing', 'Login'].map((item, index) => (
+                <li
+                  key={item}
+                  className={`bg-gradient-to-r from-indigo-500/80 from-10% via-sky-500/80 via-30% to-emerald-500/80 to-90% rounded-full transition-all duration-500 ease-out transform ${
+                    isVisible ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'
+                  }`}
+                  style={{ transitionDelay: `${index * 200}ms` }}
+                >
+                  <a href={item === 'Home' ? '/' : `/${item.toLowerCase().replace(' ', '')}`} className="block px-2 md:px-4 lg:px-8 md:py-2">
+                    {item}
+                  </a>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
       </aside>
     </div>
-  )
+  );
 }
