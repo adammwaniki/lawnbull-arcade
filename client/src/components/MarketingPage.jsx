8f66a241d63f8122e67885f57143cea8aef2a32b
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-04 10:32:02 +0300

    Lowers navbar on phones because of cutout

--- client/src/components/MarketingPage.jsx
+++ client/src/components/MarketingPage.jsx
@@ -42,10 +42,10 @@ export default function MarketingPage() {
         />
       </div>
       <div className="relative z-10 h-full overflow-y-auto overflow-x-hidden">
-        <div className="w-full md:w-1/3 md:fixed md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 flex justify-center md:justify-end z-20">
+        <div className="mt-8 w-full md:w-1/3 md:fixed md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 flex justify-center md:justify-end z-20">
           <Navbar />
         </div>
-        <div className="container mx-auto px-4 py-8 z-10 mt-16 md:mt-0 md:ml-0 md:mr-[calc(33.33%+10px)] max-w-full">
+        <div className="container mx-auto px-4 py-8 z-10 mt-7 md:mt-0 md:ml-0 md:mr-[calc(33.33%+10px)] max-w-full">
           <h1 className="text-center md:text-left text-[4rem] lg:text-[6vw] font-spicy-rice mb-8 bg-gradient-to-br from-[#A6A5A4] from-10% via-[#5855cb] via-30% to-[#66321b] to-70% text-transparent bg-clip-text">Market With Us</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:max-w-[80vw] gap-8">
             {cards.map((card) => (
