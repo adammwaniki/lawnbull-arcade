5604130b9ad849f71abd7aab16984d4bcb2fda8c
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-06 17:44:10 +0300

    Changes footer width

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex justify-center flex-wrap space-x-6 md:flex-row text-center text-xs md:text-lg p-1 bg-gray-100 text-black left-0 bottom-0 w-full" style={{ position: 'fixed', }}>
+    <footer className="flex justify-center flex-wrap space-x-6 md:flex-row text-center text-xs md:text-lg p-1 bg-gray-100 text-black left-0 bottom-0 w-screen" style={{ position: 'fixed', }}>
       <div>
         &copy; {currentYear} Lawnbull Limited. All rights reserved.
       </div>
