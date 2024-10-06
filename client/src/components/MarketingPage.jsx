d9500f7bfef0d2cb589f1d1da061dc673030e9f6
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-06 22:42:04 +0300

    Fixes marketing page footer

--- client/src/components/MarketingPage.jsx
+++ client/src/components/MarketingPage.jsx
@@ -33,21 +33,21 @@ export default function MarketingPage() {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-[#ffffff]">
-      <div className="flex-grow relative overflow-hidden">
-        <div className="absolute inset-0 z-0">
-          <Lottie 
-            animationData={onlineMarketingAnimation} 
-            loop={true}
-            autoplay={true}
-            style={{ width: '100%', height: '100%' }}
-          />
-        </div>
-        <div className="relative z-10 h-full overflow-y-auto overflow-x-hidden">
+    <>
+      <div className="fixed inset-0 z-0">
+        <Lottie 
+          animationData={onlineMarketingAnimation} 
+          loop={true}
+          autoplay={true}
+          style={{ width: '100%', height: '100%' }}
+        />
+      </div>
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <div className="flex-grow overflow-y-auto overflow-x-hidden">
           <div className="mt-8 w-full md:w-1/3 md:fixed md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 flex justify-center md:justify-end z-20">
             <Navbar />
           </div>
-          <div className="container mx-auto px-4 py-8 pb-16 z-10 mt-7 md:mt-0 md:ml-0 md:mr-[calc(33.33%+10px)] max-w-full">
+          <div className="container mx-auto px-4 py-8 z-10 mt-7 md:mt-0 md:ml-0 md:mr-[calc(33.33%+10px)] max-w-full pb-24">
             <h1 className="text-center md:text-left text-[4rem] lg:text-[6vw] font-spicy-rice mb-8 bg-gradient-to-br from-[#A6A5A4] from-10% via-[#5855cb] via-30% to-[#66321b] to-70% text-transparent bg-clip-text">Market With Us</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:max-w-[80vw] gap-8">
               {cards.map((card) => (
@@ -63,8 +63,10 @@ export default function MarketingPage() {
             <ClientCardsFull {...selectedCard} onClose={handleCloseFullCard} />
           )}
         </div>
+        <div className="relative z-10">
+          <Footer />
+        </div>
       </div>
-      <Footer />
-    </div>
-  );
+    </>
+  );    
 }
