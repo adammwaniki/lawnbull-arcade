7601c0c61d4484257aa84bdea7b52a48052a7bd0
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-06 16:29:00 +0300

    Changes card styling

--- client/src/components/cards/ClientCardsMini.jsx
+++ client/src/components/cards/ClientCardsMini.jsx
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 export default function ClientCardsMini({ image, title, subtitle, description, onViewMore }) {
   return (
     <div className="bg-[#17163e] bg-opacity-90 p-6 rounded-lg shadow-lg max-w-sm mx-auto">
-      <img src={image} alt={title} className="w-full h-48 object-cover rounded-md mb-4" />
+      <div className="aspect-w-16 aspect-h-9 mb-4">
+        <img src={image} alt={title} className="w-full h-full object-cover rounded-md" />
+      </div>
       <h2 className="text-xl font-bold text-[#fff] mb-2">{title}</h2>
       <h3 className="text-lg text-[#fff] mb-2">{subtitle}</h3>
-      <p className="text-[#fff] mb-4">{description.slice(0, 32)}...</p>
+      <p className="text-[#fff] mb-4">{description.slice(0, 64)}...</p>
       <button
         onClick={onViewMore}
-        className="w-full bg-blue-500 text-white py-2 px-4 rounded-md font-semibold uppercase tracking-wide hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 inline-block text-center"
+        className="w-full bg-gradient-to-r from-indigo-500/80 from-10% via-sky-500/80 via-30% to-emerald-500/80 to-90% text-white py-2 px-4 rounded-md font-semibold uppercase tracking-wide hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 inline-block text-center"
       >
         View More
       </button>
