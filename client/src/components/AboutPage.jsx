1cb022ae45985a34524f905fbd5fc6494f3cc454
Adam Mwaniki <adamndegwa@protonmail.com>
2024-10-10 16:55:27 +0300

    Creates About Us page

--- client/src/components/AboutPage.jsx
+++ client/src/components/AboutPage.jsx
@@ -1,12 +1,48 @@
-import Footer from "./Footer"
-import Navbar from "./Navbar"
+import { useMemo } from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import ContactCard from "./ContactCard";
+import { ParticlesLogin } from "./ParticlesLogin";
 
 export default function AboutPage() {
+  const MemoizedParticlesLogin = useMemo(() => <ParticlesLogin />, []);
+
   return (
-    <div>
-      AboutPage
-      <Navbar />
-      <Footer />
+    <div className="relative min-h-screen flex flex-col items-center justify-between">
+      {MemoizedParticlesLogin}
+      <div className="w-full md:w-1/3 md:absolute md:right-0 md:top-1/3 flex justify-center md:justify-end z-50 ">
+        <Navbar />
+      </div>
+      <main className="w-full max-w-6xl bg-opacity-80 p-4 md:p-8 rounded-lg shadow-lg z-10 flex flex-col justify-between flex-grow">
+        <div className="flex flex-col md:flex-row  md:justify-between md:h-[90vh]">
+
+          {/* Left Side Content */}
+          <div className="flex-1 max-w-md pb-6 pt-6 mt-6 bg-gray-500 bg-opacity-15 shadow-md rounded-lg p-4 md:p-6 mb-12 md:mb-0 md:mr-4  overflow-auto md:order-1 ">
+            <h3 className="text-[2rem] lg:text-[2vw] lg:leading-tight text-[#17163e] mb-4 font-spicy-rice">Growing Your Brand, One Lawn at a Time</h3>
+            <div className="text-[#17163e] space-y-5 text-md md:text-base">
+              <p>
+                At Lawnbull Limited, we specialize in marketing that helps your business thrive.
+                Our unique approach combines innovative digital strategies with impactful real-world adverts,
+                ensuring your brand stands out and connects with the right audience.
+              </p>
+              <p>
+                Our passionate team leverages the latest tools to create buzz around your brand.
+                From eye-catching social media campaigns to dynamic offline adverts, we tailor our marketing efforts
+                to maximize your visibility and engagement, all while focusing on measurable results.
+              </p>
+              <p>
+                We believe in building strong relationships with our clients. By understanding your vision and goals,
+                Lawnbull crafts personalized marketing strategies that resonate. Let&apos;s plant the seeds of success together and watch your brand flourish!
+              </p>
+            </div>
+          </div>
+          {/* Contact Card */}
+          <div className="flex-1 p-4 pb-6 pt-6 rounded-lg mb-12 md:order-2">
+            <ContactCard />
+          </div>
+        </div>
+        <Footer />
+      </main>
     </div>
-  )
+  );
 }
---
+++ client/src/components/ContactCard.jsx
@@ -0,0 +1,176 @@
+import { useState } from 'react';
+import * as emailjs from '@emailjs/browser';
+
+const ContactCard = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    businessName: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Use EmailJS to send the form data
+    emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', {
+      from_name: formData.name,
+      business_name: formData.businessName,
+      phone: formData.phone,
+      message: formData.message,
+      reply_to: formData.email, // Optionally include the user's email for replies
+    }, 'YOUR_USER_ID')
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Message sent successfully!');
+      setFormData({
+        name: '',
+        businessName: '',
+        email: '',
+        phone: '',
+        message: ''
+      });
+    }, (err) => {
+      console.error('FAILED...', err);
+      alert('Failed to send message. Please try again later.');
+    });
+  };
+
+  return (
+    <div className="max-w-sm mx-auto bg-[#17163e] bg-opacity-80   rounded-lg p-6">
+      <h2 className="text-2xl font-bold text-white mb-4">Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-white mb-1" htmlFor="name">Name:</label>
+          <input 
+            type="text" 
+            name="name" 
+            id="name" 
+            value={formData.name} 
+            onChange={handleChange} 
+            className="w-full p-2 border border-gray-300 rounded" 
+            placeholder="First and Last Name"
+            required 
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-white mb-1" htmlFor="businessName">Business Name:</label>
+          <input 
+            type="text" 
+            name="businessName" 
+            id="businessName" 
+            value={formData.businessName} 
+            onChange={handleChange} 
+            className="w-full p-2 border border-gray-300 rounded" 
+            placeholder="Business Name"
+            required 
+          />
+        </div>
+        <div className="mb-4">
+            <label className="block text-white mb-1" htmlFor="email">Email:</label>
+            <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                value={formData.email} 
+                onChange={handleChange} 
+                className="w-full p-2 border border-gray-300 rounded" 
+                placeholder="your-email@example.com"
+                required 
+            />
+        </div>
+        <div className="mb-4">
+            <label className="block text-white mb-1" htmlFor="phone">Telephone:</label>
+            <div className="flex">
+                    <select
+                    name="countryCode"
+                    id="countryCode"
+                    value={formData.countryCode}
+                    onChange={handleChange}
+                    className="w-1/2 p-2 border border-gray-300 rounded-l"
+                    >
+                    <option value="">Select</option>
+                        <option value="+254">+254 (Kenya)</option>
+                        <option value="+1">+1 (USA, Canada)</option>
+                        <option value="+43">+43 (Austria)</option>
+                        <option value="+355">+355 (Albania)</option>
+                        <option value="+32">+32 (Belgium)</option>
+                        <option value="+359">+359 (Bulgaria)</option>
+                        <option value="+91">+91 (India)</option>
+                        <option value="+98">+98 (Iran)</option>
+                        <option value="+353">+353 (Ireland)</option>
+                        <option value="+972">+972 (Israel)</option>
+                        <option value="+86">+86 (China)</option>
+                        <option value="+45">+45 (Denmark)</option>
+                        <option value="+20">+20 (Egypt)</option>
+                        <option value="+372">+372 (Estonia)</option>
+                        <option value="+34">+34 (Spain)</option>
+                        <option value="+46">+46 (Sweden)</option>
+                        <option value="+41">+41 (Switzerland)</option>
+                        <option value="+39">+39 (Italy)</option>
+                        <option value="+63">+63 (Philippines)</option>
+                        <option value="+48">+48 (Poland)</option>
+                        <option value="+351">+351 (Portugal)</option>
+                        <option value="+7">+7 (Russia, Kazakhstan)</option>
+                        <option value="+60">+60 (Malaysia)</option>
+                        <option value="+64">+64 (New Zealand)</option>
+                        <option value="+65">+65 (Singapore)</option>
+                        <option value="+81">+81 (Japan)</option>
+                        <option value="+27">+27 (South Africa)</option>
+                        <option value="+82">+82 (South Korea)</option>
+                        <option value="+44">+44 (UK)</option>
+                        <option value="+49">+49 (Germany)</option>
+                        <option value="+30">+30 (Greece)</option>
+                        <option value="+55">+55 (Brazil)</option>
+                        <option value="+62">+62 (Indonesia)</option>
+                        <option value="+66">+66 (Thailand)</option>
+                        <option value="+47">+47 (Norway)</option>
+
+                    </select>
+                    <input 
+                    type="tel" 
+                    name="phone" 
+                    id="phone" 
+                    value={formData.phone} 
+                    onChange={handleChange} 
+                    className="w-3/4 p-2 border border-gray-300 rounded-r" 
+                    placeholder="xxx-xxx-xxxx"
+                    required 
+                    />
+            </div>
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-white mb-1" htmlFor="message">Message:</label>
+          <textarea 
+            name="message" 
+            id="message" 
+            value={formData.message} 
+            onChange={handleChange} 
+            className="w-full p-2 border border-gray-300 rounded" 
+            rows="4" 
+            placeholder="Type your message here"
+            required 
+          />
+        </div>
+        <div className="mt-6 flex justify-center">
+            <button 
+            type="submit" 
+            className=" bg-green-500 text-white font-semibold py-2 px-4 rounded hover:bg-green-600"
+            >
+            Send Message
+            </button>
+        </div>
+      </form>
+      
+    </div>
+  );
+};
+
+export default ContactCard;
